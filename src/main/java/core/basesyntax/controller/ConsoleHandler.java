9fdb06d432414369131f595b1e57b8af338e12ff
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-06 14:12:52 +0200

    added from first video

---
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -0,0 +1,27 @@
+package core.basesyntax.controller;
+
+import core.basesyntax.model.Bet;
+import java.util.Scanner;
+
+public class ConsoleHandler {
+
+    public void handle() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("q")) {
+                return;
+            }
+            try {
+                String[] betData = command.split(" ");
+                int value = Integer.parseInt(betData[0]);
+                double risk = Double.parseDouble(betData[1]);
+                Bet bet = new Bet(value, risk);
+                System.out.println(bet.toString());
+            } catch (Exception e) {
+                System.out.println("pls, enter correct data");
+            }
+        }
+    }
+}
---
+++ src/main/java/core/basesyntax/model/Bet.java
@@ -0,0 +1,35 @@
+package core.basesyntax.model;
+
+public class Bet {
+    private int value;
+    private double risk;
+
+    public Bet(int value, double risk) {
+        this.value = value;
+        this.risk = risk;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public double getRisk() {
+        return risk;
+    }
+
+    public void setRisk(double risk) {
+        this.risk = risk;
+    }
+
+    @Override
+    public String toString() {
+        return "Bet{" +
+                "value=" + value +
+                ", risk=" + risk +
+                '}';
+    }
+}
