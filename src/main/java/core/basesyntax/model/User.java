c00a8be302d07e436c3fd99354e6c13b69c01b78
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-08 10:29:55 +0200

    added age, added setter

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -2,19 +2,26 @@ package core.basesyntax.model;
 
 public class User {
     private String name;
+    private int age;
 
-    User(String name) {
-        this.name = checkName(name);
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public String getName() {
         return name;
     }
 
-    private String checkName(String name) {
-        if (name == null || name.endsWith("")) {
-            throw new RuntimeException("Wrong name");
-        }
-        return name;
-    }
 }
