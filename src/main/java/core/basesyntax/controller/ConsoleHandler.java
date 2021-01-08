1684a5c53619df46ed4b57b76b75f5090b7de31d
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-08 10:29:38 +0200

    added check for value, risk and bet

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -21,12 +21,19 @@ public class ConsoleHandler {
                 String[] betData = command.split(" ");
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
+                if (value <= 0 || risk <= 0) {
+                    throw new RuntimeException();
+                }
                 bet = new Bet(value, risk);
             } catch (NumberFormatException e) {
-                System.out.println("pls, enter correct data");
+                System.out.println("Please, enter correct data");
+            } catch (RuntimeException e) {
+                System.out.println("Value and risk can not be zero or less");
             }
-            betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
+            if (bet != null) {
+                betDao.add(bet);
+                System.out.println(bet.toString());
+            }
         }
     }
 }
