201accb27bbb87dcc37a97dcd3022d7654f9a988
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-01-11 16:28:54 +0200

    clean package change

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,9 +4,9 @@ import core.basesyntax.controller.ConsoleHandler;
 import core.basesyntax.library.Injector;
 import java.lang.reflect.InvocationTargetException;
 
-
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter value and risk for your bet");
         handler.handle();
--- src/main/java/core/basesyntax/factory/Factory.java
+++ src/main/java/core/basesyntax/factory/Factory.java
@@ -9,14 +9,14 @@ public class Factory {
     private static BetDao betDao;
     private static UserDao userDao;
 
-    public static BetDao getBetDao(){
+    public static BetDao getBetDao() {
         if (betDao == null) {
             betDao = new BetDaoImpl();
         }
         return betDao;
     }
 
-    public static UserDao getUserDao(){
+    public static UserDao getUserDao() {
         if (userDao == null) {
             userDao = new UserDaoImpl();
         }
